42b23860e1954114c1be4558803d00043fe36275
Nicolae Pop <nicupop729@gmail.com>
2021-12-09 23:54:21 -0300

    feat: style the web app

--- src/components/RemoveBook.js
+++ src/components/RemoveBook.js
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { deleteBookRedux } from '../redux/books/booksRedux';
+import './removeBook.css';
 
 const RemoveBook = ({ bookId }) => {
   const dispatch = useDispatch();
@@ -8,7 +9,7 @@ const RemoveBook = ({ bookId }) => {
   const handleRemove = () => dispatch(deleteBookRedux(bookId));
 
   return (
-    <button type="button" onClick={handleRemove}>
+    <button type="button" className="remove-btn" onClick={handleRemove}>
       Remove
     </button>
   );
